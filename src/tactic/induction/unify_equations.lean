41665e23271946747ab0ec88b72813ac919f55ce
Jannis Limperg <jannis@limperg.de>
2020-08-06 21:48:23 +0200

    Restructure and document unify_equations

--- src/tactic/induction/unify_equations.lean
+++ src/tactic/induction/unify_equations.lean
@@ -11,15 +11,45 @@ open expr
 namespace tactic
 namespace unify_equations
 
-meta inductive simplification_result
+/--
+The result of a unification step:
+
+- `simplified hs` means that the step succeeded and produced new (simpler)
+  equations `hs`. `hs` can be empty.
+- `goal_solved` means that the step succeeded and solved the goal (by deriving a
+  contradiction from the given equation).
+- `not_simplified` means that the step failed to simplify the equation.
+-/
+meta inductive unification_step_result
 | simplified (next_equations : list name)
 | not_simplified
 | goal_solved
 
-export simplification_result
+export unification_step_result
 
-meta def unify_heterogeneous (equ lhs_type rhs_type lhs rhs : expr) :
-  tactic simplification_result :=
+/--
+A unification step is a tactic that attempts to simplify a given equation and
+returns a `unification_step_result`. The inputs are:
+
+- `equ`, the equation being processed. Must be a local constant.
+- `lhs_type` and `rhs_type`, the types of equ's LHS and RHS. For homogeneous
+  equations, these are defeq.
+- `lhs` and `rhs`, `equ`'s LHS and RHS.
+- `lhs_whnf` and `rhs_whnf`, `equ`'s LHS and RHS in WHNF.
+- `u`, `equ`'s level.
+
+So `equ : @eq.{u} lhs_type lhs rhs` or `equ : @heq.{u} lhs_type lhs rhs_type rhs`.
+-/
+@[reducible] meta def unification_step :=
+  ∀ (equ lhs_type rhs_type lhs rhs lhs_whnf rhs_whnf : expr) (u : level),
+  tactic unification_step_result
+
+/--
+For `equ : t == u` with `t : T` and `u : U`, if `T` and `U` are defeq,
+we replace `equ` with `equ : t = u`.
+-/
+meta def unify_heterogeneous : unification_step :=
+λ equ lhs_type rhs_type lhs rhs _ _ _,
 do {
   is_def_eq lhs_type rhs_type,
   p ← to_expr ``(@eq_of_heq %%lhs_type %%lhs %%rhs %%equ),
@@ -29,8 +59,11 @@ do {
 } <|>
 pure not_simplified
 
-meta def unify_defeq (equ type lhs rhs lhs_whnf rhs_whnf : expr)
-  (u : level) : tactic simplification_result :=
+/--
+For `equ : t = u`, if `t` and `u` are defeq, we delete `equ`.
+-/
+meta def unify_defeq : unification_step :=
+λ equ lhs_type _ _ _ lhs_whnf rhs_whnf _,
 do {
   is_def_eq lhs_whnf rhs_whnf,
   clear equ,
@@ -38,8 +71,12 @@ do {
 } <|>
 pure not_simplified
 
-meta def unify_var (equ type lhs rhs lhs_whnf rhs_whnf : expr)
-  (u : level) : tactic simplification_result :=
+/--
+For `equ : x = t` or `equ : t = x`, where `x` is a local constant, we
+substitute `x` with `t` in the goal.
+-/
+meta def unify_var : unification_step :=
+λ equ type _ lhs rhs lhs_whnf rhs_whnf u,
 do {
   let lhs_is_local := lhs_whnf.is_local_constant,
   let rhs_is_local := rhs_whnf.is_local_constant,
@@ -55,11 +92,66 @@ do {
 } <|>
 pure not_simplified
 
+-- TODO copied from core (init/meta/injection_tactic.lean)
+meta def injection_with (h : expr) (ns : list name)
+  (base := `h) (offset := some 1) : tactic (list expr × list name) :=
+do
+  H ← infer_type h,
+  (lhs, rhs, constructor_left, constructor_right, inj_name) ← do {
+    (lhs, rhs) ← match_eq H,
+    lhs ← whnf_ginductive lhs,
+    rhs ← whnf_ginductive rhs,
+    env ← get_env,
+    (const constructor_left _) ← pure $ get_app_fn lhs,
+    (const constructor_right _) ← pure $ get_app_fn rhs,
+    inj_name ← resolve_constant $ constructor_left ++ "inj_arrow",
+    pure (lhs, rhs, constructor_left, constructor_right, inj_name)
+  } <|> fail
+    "injection tactic failed, argument must be an equality proof where lhs and rhs are of the form (c ...), where c is a constructor",
+  if constructor_left = constructor_right then do
+    inj ← mk_const inj_name,
+    inj_type ← infer_type inj,
+    inj_arity ← get_pi_arity inj_type,
+    let num_equations :=
+      (inj_type.nth_binding_body (inj_arity - 1)).binding_domain.pi_arity,
+    tgt ← target,
+    proof ← mk_mapp inj_name (list.repeat none (inj_arity - 3) ++ [some h, some tgt]),
+    eapply proof,
+    intron_with num_equations ns base offset
+  else do
+    tgt ← target,
+    let inductive_name := constructor_left.get_prefix,
+    pr ← mk_app (inductive_name <.> "no_confusion") [tgt, lhs, rhs, h],
+    exact pr,
+    return ([], ns)
+
+/--
+Given `equ : C x₁ ... xₙ = D y₁ ... yₘ` with `C` and `D` constructors of the
+same datatype `I`:
+
+- If `C ≠ D`, we solve the goal by contradiction using the no-confusion rule.
+- If `C = D`, we clear `equ` and add equations `x₁ = y₁`, ..., `xₙ = yₙ`.
+-/
+meta def unify_constructor_headed : unification_step :=
+λ equ _ _ _ _ _ _ _,
+do {
+  (next, _) ← injection_with equ [] `_ none,
+  try $ clear equ,
+  pure $
+    if next.empty
+      then goal_solved
+      else simplified $ next.map expr.local_pp_name
+} <|>
+pure not_simplified
+
+/--
+For `type = I x₁ ... xₙ`, where `I` is an inductive type, `get_sizeof type`
+returns the constant `I.sizeof`. Fails if `type` is not of this form or if no
+such constant exists.
+-/
 meta def get_sizeof (type : expr) : tactic pexpr := do
   n ← get_inductive_name type,
-  let sizeof_name := n ++ `sizeof,
-  sizeof ← resolve_name $ sizeof_name,
-  pure sizeof
+  resolve_name $ n ++ `sizeof
 
 lemma plus_gt (n m : ℕ) : m ≠ 0 → n + m > n :=
 by { induction m, { contradiction }, { simp } }
@@ -68,6 +160,11 @@ lemma n_plus_m_plus_one_ne_n (n m : ℕ) : n + (m + 1) ≠ n :=
 by simp [ne_of_gt, plus_gt]
 -- Linarith could prove this, but I want to avoid that dependency.
 
+/--
+`match_n_plus_m n e` matches `e` of the form `nat.succ (... (nat.succ e')...)`.
+It returns `n` plus the number of `succ` constructors and `e'`. The matching is
+performed up to normalisation with transparency `md`.
+-/
 meta def match_n_plus_m (md) : ℕ → expr → tactic (ℕ × expr) :=
 λ n e, do
   e ← whnf e md,
@@ -76,6 +173,13 @@ meta def match_n_plus_m (md) : ℕ → expr → tactic (ℕ × expr) :=
   | _ := pure (n, e)
   end
 
+/--
+Given `equ : n + m = n` or `equ : n = n + m` with `n` and `m` natural numbers
+and `m` a nonzero literal, this tactic produces a proof of `false`. More
+precisely, the two sides of the equation must be of the form
+`nat.succ (... (nat.succ e)...)` with different numbers of `nat.succ`
+constructors. Matching is performed with transparency `md`.
+-/
 meta def contradict_n_eq_n_plus_m (md : transparency) (equ lhs rhs : expr) :
   tactic expr := do
   ⟨lhs_n, lhs_e⟩ ← match_n_plus_m md 0 lhs,
@@ -98,8 +202,12 @@ meta def contradict_n_eq_n_plus_m (md : transparency) (equ lhs rhs : expr) :
   let m := reflect (diff - 1),
   pure `(n_plus_m_plus_one_ne_n %%n %%m %%equ)
 
-meta def unify_cyclic (equ type lhs rhs lhs_whnf rhs_whnf : expr)
-  (u : level) : tactic simplification_result :=
+/--
+Given `equ : t = u` with `t, u : I` and `I.sizeof t ≠ I.sizeof u`, we solve the
+goal by contradiction.
+-/
+meta def unify_cyclic : unification_step :=
+λ equ type _ _ _ lhs_whnf rhs_whnf _,
 do {
   -- Establish `sizeof lhs = sizeof rhs`.
   sizeof ← get_sizeof type,
@@ -118,102 +226,70 @@ do {
 } <|>
 pure not_simplified
 
--- TODO copied from core (init/meta/injection_tactic.lean)
-meta def injection_with (h : expr) (ns : list name)
-  (base := `h) (offset := some 1) : tactic (list expr × list name) :=
+/--
+Sequence two unification steps. First run step `s`. If this was successful
+(i.e. it simplified or solved the goal), stop and return the result of `s`.
+Otherwise run `t` and return its result.
+-/
+meta def sequence_simplifiers (s t : unification_step) : unification_step :=
+λ equ lhs_type rhs_type lhs rhs lhs_whnf rhs_whnf u,
 do
-  H ← infer_type h,
-  (lhs, rhs, constructor_left, constructor_right, inj_name) ← do {
-    (lhs, rhs) ← match_eq H,
-    lhs ← whnf_ginductive lhs,
-    rhs ← whnf_ginductive rhs,
-    env ← get_env,
-    (const constructor_left _) ← pure $ get_app_fn lhs,
-    (const constructor_right _) ← pure $ get_app_fn rhs,
-    inj_name ← resolve_constant $ constructor_left ++ "inj_arrow",
-    pure (lhs, rhs, constructor_left, constructor_right, inj_name)
-  } <|> fail
-    "injection tactic failed, argument must be an equality proof where lhs and rhs are of the form (c ...), where c is a constructor",
-  if constructor_left = constructor_right then do
-    -- C.inj_arrow, for a given constructor C of datatype D, has type
-    --
-    --     ∀ (A₁ ... Aₙ) (x₁ ... xₘ) (y₁ ... yₘ), C x₁ ... xₘ = C y₁ ... yₘ
-    --       → ∀ ⦃P : Sort u⦄, (x₁ = y₁ → ... → yₖ = yₖ → P) → P
-    --
-    -- where the Aᵢ are parameters of D and the xᵢ/yᵢ are arguments of C.
-    -- Note that if xᵢ/yᵢ are propositions, no equation is generated, so the
-    -- number of equations is not necessarily the constructor arity.
-
-    -- First, we find out how many equations we need to intro later.
-    inj ← mk_const inj_name,
-    inj_type ← infer_type inj,
-    inj_arity ← get_pi_arity inj_type,
-    let num_equations :=
-      (inj_type.nth_binding_body (inj_arity - 1)).binding_domain.pi_arity,
-
-    -- Now we generate the actual proof of the target.
-    tgt ← target,
-    proof ← mk_mapp inj_name (list.repeat none (inj_arity - 3) ++ [some h, some tgt]),
-    eapply proof,
-    intron_with num_equations ns base offset
-  else do
-    tgt ← target,
-    let inductive_name := constructor_left.get_prefix,
-    pr ← mk_app (inductive_name <.> "no_confusion") [tgt, lhs, rhs, h],
-    exact pr,
-    return ([], ns)
-
-meta def unify_constructor_headed (equ type lhs rhs lhs_whnf rhs_whnf : expr)
-  (u : level) : tactic simplification_result :=
-do {
-  (next, _) ← injection_with equ [] `_ none,
-  try $ clear equ,
-  pure $
-    if next.empty
-      then goal_solved
-      else simplified $ next.map expr.local_pp_name
-} <|>
-pure not_simplified
-
-meta def sequence_simplifiers (s t : tactic simplification_result) :
-  tactic simplification_result := do
-  r ← s,
+  r ← s equ lhs_type rhs_type lhs rhs lhs_whnf rhs_whnf u,
   match r with
   | simplified _ := pure r
   | goal_solved := pure r
-  | not_simplified := t
+  | not_simplified := t equ lhs_type rhs_type lhs rhs lhs_whnf rhs_whnf u
   end
 
-meta def unify_homogeneous (equ type lhs rhs lhs_whnf rhs_whnf : expr)
-  (u : level) : tactic simplification_result := do
-  list.foldl sequence_simplifiers (pure not_simplified)
-    [ unify_defeq equ type lhs rhs lhs_whnf rhs_whnf u,
-      unify_var equ type lhs rhs lhs_whnf rhs_whnf u,
-      unify_constructor_headed equ type lhs rhs lhs_whnf rhs_whnf u,
-      unify_cyclic equ type lhs rhs lhs_whnf rhs_whnf u ]
+/--
+For `equ : t = u`, try the following methods in order: `unify_defeq`,
+`unify_var`, `unify_constructor_headed`, `unify_cyclic`. If any of them is
+successful, stop and return its result. If none is successful, fail.
+-/
+meta def unify_homogeneous : unification_step :=
+list.foldl sequence_simplifiers (λ _ _ _ _ _ _ _ _, pure not_simplified)
+  [unify_defeq, unify_var, unify_constructor_headed, unify_cyclic]
 
 end unify_equations
 
 
 open unify_equations
 
-meta def unify_equation (equ : name) : tactic simplification_result := do
+/--
+If `equ` is the display name of a local constant with type `t = u` or `t == u`,
+then `unify_equation_once equ` simplifies it once using
+`unify_equations.unify_homogeneous` or `unify_equations.unify_heterogeneous`.
+
+Otherwise it fails.
+-/
+meta def unify_equation_once (equ : name) : tactic unification_step_result := do
   eque ← get_local equ,
   t ← infer_type eque,
   match t with
   | (app (app (app (const `eq [u]) type) lhs) rhs) := do
     lhs_whnf ← whnf_ginductive lhs,
     rhs_whnf ← whnf_ginductive rhs,
-    unify_homogeneous eque type lhs rhs lhs_whnf rhs_whnf u
-  | `(@heq %%lhs_type %%lhs %%rhs_type %%rhs) := do
-    unify_heterogeneous eque lhs_type rhs_type lhs rhs
+    unify_homogeneous eque type type lhs rhs lhs_whnf rhs_whnf u
+  | (app (app (app (app (const `heq [u]) lhs_type) lhs) rhs_type) rhs) := do
+    lhs_whnf ← whnf_ginductive lhs,
+    rhs_whnf ← whnf_ginductive rhs,
+    unify_heterogeneous eque lhs_type rhs_type lhs rhs lhs_whnf rhs_whnf u
   | _ := fail! "Expected {equ} to be an equation, but its type is\n{t}."
   end
 
+/--
+Given a list of display names of local hypotheses that are (homogeneous or
+heterogeneous) equations, `unify_equations` simplifies them recursively using
+`unify_equations.unify_homogeneous` or `unify_equations.unify_heterogeneous`.
+The recursion is depth-first, so all unification steps are applied to the first
+equation (repeatedly) until no unification step can simplify it further.
+
+Returns true iff the goal has been solved by contradiction.
+-/
 meta def unify_equations : list name → tactic bool
 | [] := pure ff
 | (h :: hs) := do
-  res ← unify_equation h,
+  res ← unify_equation_once h,
   match res with
   | simplified hs' := unify_equations $ hs' ++ hs
   | not_simplified := unify_equations hs
@@ -225,7 +301,7 @@ namespace interactive
 
 open lean.parser
 
-meta def simplify_index_equations (eqs : interactive.parse (many ident)) :
+meta def unify_equations (eqs : interactive.parse (many ident)) :
   tactic unit :=
 tactic.unify_equations eqs *> skip
 
