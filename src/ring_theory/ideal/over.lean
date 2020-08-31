fa76944ed2940e5ca4b2d0d781bcb06505937712
Devon Tuma <tumax040@umn.edu>
2020-08-31 12:35:39 -0500

    Fill in most of the proof

--- src/ring_theory/ideal/over.lean
+++ src/ring_theory/ideal/over.lean
@@ -168,6 +168,8 @@ end integral_domain
 
 section going_up
 
+local attribute [instance] classical.prop_decidable
+
 variables {S : Type*} [comm_ring S] [algebra R S]
 
 /-- `comap (algebra_map R S)` is a surjection from the prime spec of `R` to prime spec of `S` -/
@@ -178,20 +180,34 @@ begin
   let f := localization.of P.prime_compl,
   let Sₚ := localization (algebra_map_submonoid S P.prime_compl),
   let g := localization.of (algebra_map_submonoid S P.prime_compl),
-  haveI : algebra Rₚ Sₚ := localization_algebra P.prime_compl f g,
+  let ϕ := (@algebra_map Rₚ Sₚ _ _ (localization_algebra P.prime_compl f g)),
+  --haveI : algebra Rₚ Sₚ := localization_algebra P.prime_compl f g,
   let Pₚ := local_ring.maximal_ideal Rₚ,
   -- haveI : is_maximal Pₚ := local_ring.is_max
-  by_cases hPₚ : map (algebra_map Rₚ Sₚ) Pₚ = ⊤,
+  by_cases hPₚ : map ϕ Pₚ = ⊤,
   {
-    exfalso, sorry,
+    exfalso,
+    sorry,
   },
   {
     obtain ⟨Qₚ : ideal Sₚ, ⟨Qₚ_maximal, hQₚ⟩⟩ := exists_le_maximal _ hPₚ,
-    use comap g.to_map Qₚ,
-    have h₁ : Pₚ ≤ comap (algebra_map Rₚ Sₚ) Qₚ := le_trans le_comap_map (comap_mono hQₚ),
-    have h₂ : comap (algebra_map Rₚ Sₚ) Qₚ ≠ ⊤ := comap_ne_top _ Qₚ_maximal.1,
+    haveI : is_maximal Qₚ := Qₚ_maximal,
+    have h₁ : Pₚ ≤ comap ϕ Qₚ := le_trans le_comap_map (comap_mono hQₚ),
+    have h₂ : comap ϕ Qₚ ≠ ⊤ := comap_ne_top _ Qₚ_maximal.1,
     have : Pₚ = comap _ Qₚ := is_maximal.eq_of_le (local_ring.maximal_ideal.is_maximal Rₚ) h₂ h₁,
-
+    refine ⟨comap g.to_map Qₚ, ⟨_, _⟩⟩,
+    {
+      refine comap_is_prime g.to_map Qₚ,
+    },
+    {
+      suffices : P = comap ((@algebra_map Rₚ Sₚ _ _ (localization_algebra P.prime_compl f g)).comp f.to_map) Qₚ, {
+        rw comap_comap,
+        erw localization_map.map_comp at this,
+        exact this,
+      },
+      rw [← comap_comap, ← this],
+      exact (localization.at_prime_comap_maximal_ideal P).symm,
+    }
   }
 end
 
